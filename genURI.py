b1338f951282ac84d8b2fe214f543f068cf9996c
MarcelTs <x.joo@hotmail.com>
2015-11-17 18:27:02 +0100

    Generated output for program + parsing XML, awaiting Alchemy part

--- genURI.py
+++ genURI.py
@@ -1,12 +1,24 @@
+import xml.etree.ElementTree
 import requests, sys, json
 
 googleAPIKey = "AIzaSyB23UnDXR2PyYdSygH1ClmUvIHvrdwacDo"
 searchEngineKey = "016723847753961302155:y6-cneh1knc"
+googleSearchURL = "https://www.googleapis.com/customsearch/v1"
+googleSearchExampleFile = "exampleResponse.json"
+
+dbpediaSpotlightURL = "http://spotlight.dbpedia.org/rest/annotate"
+spotlightExampleFile = "spotlightResponseExample.xml"
+
+'''
+============================================================================
+PART 1 : Send a query and retrieve list of URLs
+============================================================================
+'''
 
 def getURLSfromQuery(query):
 
-  #jsonContent = getJSONFromWeb(query)
-  jsonContent = getJSONFromFile()
+  #jsonContent = getSearchFromWeb(query, true)
+  jsonContent = getSearchFromFile()
 
   jsonObject = json.loads(jsonContent)
 
@@ -17,27 +29,140 @@ def getURLSfromQuery(query):
 
   return urls;
 
-def getJSONFromFile():
-  with open("exampleResponse.json", "r") as myfile:
+def getSearchFromFile():
+  with open(googleSearchExampleFile, "r") as myfile:
     jsonContent = myfile.read()
 
   return jsonContent
 
-def getJSONFromWeb(query):
-  response = requests.get("https://www.googleapis.com/customsearch/v1?q=" + query + "&key=" + googleAPIKey + "&cx= " + searchEngineKey)
+def getSearchFromWeb(query, writeToFile):
+  payload = {
+    "query": query,
+    "key": googleAPIKey,
+    "cx": searchEngineKey
+  }
+
+  response = requests.get(googleSearchURL, params = payload)
   jsonContent = response.json()
 
-  writeJSONToFile(jsonContent)
+  if(writeToFile):
+    writeContentToFile(googleSearchExampleFile, jsonContent)
 
   return jsonContent
 
-def writeJSONToFile(json):
-  with open("exampleResponse.json", "a") as jsonFile:
-    jsonFile.write(json)
+def writeContentToFile(fileName, content):
+  with open(googleSearchExampleFile, "a") as jsonFile:
+    jsonFile.write(content)
+
+'''
+============================================================================
+PART 2 : For each URL, use Alchemy to extract text and semantic data
+============================================================================
+'''
+
+
+'''
+============================================================================
+PART 3 : For each snippet of text, enhance with DBpedia Spotlight (annotate)
+============================================================================
+'''
+
+def getURIsFromTexts(texts, spotlightConfidence, spotlightSupport):
+  annotatedTexts = {}
+  for url, text in texts.items():
+    uris = getURIsFromText(text, spotlightConfidence, spotlightSupport)
+    annotatedTexts[url] = uris
+
+  return annotatedTexts
+
+def getURIsFromText(text, spotlightConfidence, spotlightSupport):
+  content = getAnnotatedTextFromFile()
+
+  # Extract URI
+  uris = getDBPediaRessources(content)
+
+  return uris
+
+def getAnnotatedTextFromFile():
+  with open(spotlightExampleFile, "r") as myfile:
+    content = myfile.read()
+
+  return content
+
+
+def getAnnotatedTextFromWeb(text, spotlightConfidence, spotlightSupport):
+  payload = {
+    "text": text,
+    "confidence": spotlightConfidence,
+    "support": spotlightSupport
+    #"sparql":
+  }
+
+  response = requests.get(dbpediaSpotlightURL, params = payload)
+
+  content = response.text
+    
+  if(writeToFile):
+    writeContentToFile(spotlightExampleFile, content)
+
+  return content
+
+def getDBPediaRessources(xmlRawContent):
+  xmlRoot = xml.etree.ElementTree.fromstring(xmlRawContent)
+  xmlRoot = xmlRoot.find("Resources")
+  resources = xmlRoot.findall("Resource")
+  
+  uris = []
+  for resource in resources:
+    uri = resource.get("URI")
+    uris.append(uri)
+
+  return uris
+
+
+'''
+============================================================================
+MAIN
+============================================================================
+'''
 
 def main():
   query = sys.argv[1]
-  getURLSfromQuery(query)
+
+  # Default values for spotlightConfidence is 0.2 and for spotlightSupport is 20
+  if(2 < len(sys.argv)):
+    spotlightConfidence = sys.argv[2]
+  else:
+    spotlightConfidence = 0.2
+
+  if(3 < len(sys.argv)):
+    spotlightSupport = sys.argv[3]
+  else:
+    spotlightSupport = 20
+
+  # Retrieve URLS based on query
+  urls = getURLSfromQuery(query)
+
+  # Retrieve, for each URL, an associated text
+  texts = {
+      urls[0]: "Inception"
+    }
+
+  # Retrieve, for each text, the list of corresponding URIs
+  annotatedTexts = getURIsFromTexts(texts, spotlightConfidence, spotlightSupport)
+
+
+  # Prepare the JSON
+  response = []
+  for url, uris in annotatedTexts.items():
+    response.append({
+        "url": url,
+        "uri": uris
+      })
+
+  jsonResponse = json.dumps(response)
+
+  print(jsonResponse)
 
 if  __name__ =='__main__':
   main()
