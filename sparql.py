2bc5828cc3d5643548572325086161fb76df3ca0
Aurélien Coussat <aurelien.coussat@insa-lyon.fr>
2015-11-20 10:36:57 +0100

    Lecture d'un fichier d'entrée

--- sparql.py
+++ sparql.py
@@ -1,14 +1,28 @@
-import requests
+import requests, sys, json
 
 def main():
 
     dbpediaEndpoint = "http://dbpedia.org/sparql"
+    inputURIs = "sample.json"
+
+
+    # Lecture des URIs
+
+    with open(inputURIs, "r") as myfile:
+        jsonContent = myfile.read()
+    jsonObject = json.loads(jsonContent)
+
+    for uri in jsonObject["uris"]:
+        print(uri["url"] + " --- " + str(uri["uri"]))
+
+
+    # Requête SPARQL
 
     payload = {
         "query": """SELECT DISTINCT ?Concept
                     WHERE {[] a ?Concept}
                     LIMIT 100""",
-        "format": "application/sparql-results+json",
+        "format": "json",
         "timeout": "30000"
     }
 
