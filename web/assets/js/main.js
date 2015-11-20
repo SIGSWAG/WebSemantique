960b9dec089fefcca167f474884892051f81a654
Loïc Touzard <ltouzard@degustandco.com>
2015-11-20 11:49:59 +0100

    Coloration json, ajout du sample json, correction requete server dans le js

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,9 +1,36 @@
+function displayResults(jsonResponse){
+
+}
+
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 4);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+
 $(function(){
 	$("#search").submit(function(e){
 		e.preventDefault();
 		$this = $(this);
 
-		var path = "./cgi-bin/main.cgi";
+		var path = "./cgi-bin/server.py";
 		var params = {};
 
 		params.mots_clefs = $("#searchText").val();
@@ -14,7 +41,8 @@ $(function(){
 			url: path,
 			data: $.param(params),
 			success : function(json, statut){
-
+				$("#results").append('<pre class="json">'+syntaxHighlight(json)+'</pre>');
+				console.log(syntaxHighlight(json));
 			},
 			error: function (resultat, statut, erreur) {
 				console.log("error");
@@ -23,7 +51,6 @@ $(function(){
 			},
 			complete: function(response){
 				console.log(response);
-				$("#results").append("<pre>"+response.responseText+"</pre>");
 			}
 		});
 		return false;
