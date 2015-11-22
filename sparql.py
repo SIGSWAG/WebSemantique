b4772da3d57bf8be1bd14b93e04a7fc8250d38a8
RastaMarcusLeRouge <marc.javin@insa-lyon.fr>
2015-11-22 19:58:50 +0100

    mise en place de la structure de sortie

--- sparql.py
+++ sparql.py
@@ -7,14 +7,21 @@ outputFileName = "output.json"
 
 def requete(listURI):
 # Requête SPARQL
+
+	struct = {}
+	struct['link'] = 'http:zbbooob'
+	struct['results'] = {}
+	struct['results']['graphePage'] = []
+	struct['results']['films'] = []
+	struct['results']['films']
+	
+
 	payload = {
 		"query": """SELECT DISTINCT ?s ?p ?o
 					WHERE {
 						
 						?s a <http://dbpedia.org/ontology/Film>.
 						?s ?p ?o.
-						?s ?nimp ?uri.
-						FILTER (?uri IN """ + listURI + """).
 						
 					} LIMIT 250
 					OFFSET 5000""",
@@ -25,9 +32,19 @@ def requete(listURI):
 	response = requests.get(dbpediaEndpoint, params = payload)
 
 	responseJson = response.json()
+	
+	graphe = []
+	
+	jacky = {}
+	jacky['link']='toto'
+	jacky['graphe'] = responseJson['results']['bindings']
+
+	struct['results']['films'].append(jacky)
+	
+	
 
 	with open(outputFileName, "w") as myfile:
-		myfile.write(json.dumps(responseJson))
+		myfile.write(json.dumps(struct))
 
 def main():
 
@@ -41,15 +58,15 @@ def main():
 
 	for page in jsonObject["pages"]:
 		listURI = "("
-		print(page["url"] + " --- " + str(page["uri"]))
+		#print(page["url"] + " --- " + str(page["uri"]))
 		for uri in page["uri"]:
 			listURI += "\"" + uri + "\","
 		listURI=listURI[0:-1] + ")"
-		print(listURI)
+		#print(listURI)
 		requete(listURI)
 
 
-	print(listURI)
+	#print(listURI)
 	# Requête SPARQL
 
 
