cd7f990c3e645890448ed21bddcef1e09b1e825b
RastaMarcusLeRouge <marc.javin@insa-lyon.fr>
2015-11-22 16:03:12 +0100

    Requete calmement et décode le JSON

--- sparql.py
+++ sparql.py
@@ -1,40 +1,59 @@
-import requests, sys, json
+import requests, sys, json, codecs
+
+
+dbpediaEndpoint = "http://live.dbpedia.org/sparql"
+inputURIs = "sample.json"
+outputFileName = "output.json"
+
+def requete(listURI):
+# Requête SPARQL
+	payload = {
+		"query": """SELECT DISTINCT ?s ?p ?o
+					WHERE {
+						
+						?s a <http://dbpedia.org/ontology/Film>.
+						?s ?p ?o.
+						?s ?nimp ?uri.
+						FILTER (?uri IN """ + listURI + """).
+						
+					} LIMIT 250
+					OFFSET 5000""",
+		"format": "json",
+		"timeout": "30000"
+	}
+
+	response = requests.get(dbpediaEndpoint, params = payload)
+
+	responseJson = response.json()
+
+	with open(outputFileName, "w") as myfile:
+		myfile.write(json.dumps(responseJson))
 
 def main():
 
-    dbpediaEndpoint = "http://live.dbpedia.org/sparql"
-    inputURIs = "sample.json"
-    outputFileName = "output.json"
+	# Lecture des URIs
 
-    # Lecture des URIs
+	with open(inputURIs, "r") as myfile:
+		jsonContent = myfile.read()
+	jsonObject = json.loads(jsonContent)
 
-    with open(inputURIs, "r") as myfile:
-        jsonContent = myfile.read()
-    jsonObject = json.loads(jsonContent)
+	
 
-    for uri in jsonObject["uris"]:
-        print(uri["url"] + " --- " + str(uri["uri"]))
+	for page in jsonObject["pages"]:
+		listURI = "("
+		print(page["url"] + " --- " + str(page["uri"]))
+		for uri in page["uri"]:
+			listURI += "\"" + uri + "\","
+		listURI=listURI[0:-1] + ")"
+		print(listURI)
+		requete(listURI)
 
 
-    # Requête SPARQL
+	print(listURI)
+	# Requête SPARQL
 
-    payload = {
-        "query": """SELECT DISTINCT *
-                    WHERE {
-                        ?film rdf:type <http://dbpedia.org/ontology/Film> .
-                        ?film ?prop ?propval
-                    } LIMIT 250
-                    OFFSET 5000""",
-        "format": "json",
-        "timeout": "30000"
-    }
 
-    response = requests.get(dbpediaEndpoint, params = payload)
-    responseJson = json.loads(response.text)
 
-    # TODO transformer le JSON dans le format approprié
 
-    with open(outputFileName, "w") as myfile:
-        myfile.write(json.dumps(responseJson))
 
 main()
