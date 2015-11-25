799f30484bbb938511a75364bb6aa71b65d2a4c7
Loïc Touzard <ltouzard@degustandco.com>
2015-11-25 16:53:12 +0100

    Ajout de la barre de chargement synchronisée. Simplification css, bye bye les prefix !

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,3 +1,5 @@
+var jsonSample = [{"link": "http://www.imdb.com/title/tt1375666/", "results": {"graphePage": [{"o": {"value": "http://www.w3.org/2002/07/owl#Thing", "type": "uri"}, "p": {"value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}, {"o": {"value": "http://fr.dbpedia.org/resource/R\u00eave", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}, {"o": {"value": "http://de.dbpedia.org/resource/Traum", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}, {"o": {"value": "http://es.dbpedia.org/resource/Ensue\u00f1o", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}, {"o": {"value": "http://eu.dbpedia.org/resource/Amets", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}, {"o": {"value": "http://id.dbpedia.org/resource/Mimpi", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}, {"o": {"value": "http://it.dbpedia.org/resource/Sogno", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}, {"o": {"value": "http://ja.dbpedia.org/resource/\u5922", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}, {"o": {"value": "http://ko.dbpedia.org/resource/\uafc8", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}, {"o": {"value": "http://nl.dbpedia.org/resource/Droom", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}}], "films": [{"graphe": [{"o": {"value": "http://schema.org/Movie", "type": "uri"}, "p": {"value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://schema.org/CreativeWork", "type": "uri"}, "p": {"value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://www.w3.org/2002/07/owl#Thing", "type": "uri"}, "p": {"value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/ontology/Film", "type": "uri"}, "p": {"value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/ontology/Wikidata:Q11424", "type": "uri"}, "p": {"value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/ontology/Work", "type": "uri"}, "p": {"value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://www.wikidata.org/entity/Q386724", "type": "uri"}, "p": {"value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://rdf.freebase.com/ns/m.0661ql3", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://wikidata.dbpedia.org/resource/Q25188", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://fr.dbpedia.org/resource/Inception", "type": "uri"}, "p": {"value": "http://www.w3.org/2002/07/owl#sameAs", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/Art", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/Dream", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/The_Fugitive_%28TV_series%29", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/I_Love_New_York_%28TV_series%29", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/Salvation", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/Prediction", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/Quantity", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/Rare_Ltd.", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}, {"o": {"value": "http://dbpedia.org/resource/Vulnerable_species", "type": "uri"}, "p": {"value": "http://blabla.com/relationTheme", "type": "uri"}, "s": {"value": "http://dbpedia.org/resource/Inception", "type": "uri"}}], "link": "http://dbpedia.org/resource/Inception", "infos": {"country": {"value": "United States", "xml:lang": "en", "type": "literal"}, "director": {"value": "http://dbpedia.org/resource/Christopher_Nolan", "type": "uri"}, "starring": {"value": "* Leonardo DiCaprio\n* Ken Watanabe\n* Joseph Gordon-Levitt\n* Marion Cotillard\n* Ellen Page\n* Tom Hardy\n* Cillian Murphy\n* Tom Berenger\n* Michael Caine", "xml:lang": "en", "type": "literal"}, "name": {"value": "Inception", "xml:lang": "en", "type": "literal"}, "dirName": {"value": "Christopher Nolan", "xml:lang": "en", "type": "literal"}}}]}}, {"link": "http://www.imdb.com/title/tt0097178/", "results": {"graphePage": [], "films": []}}];
+
 function syntaxHighlight(json) {
     if (typeof json != 'string') {
          json = JSON.stringify(json, undefined, 4);
@@ -32,6 +34,9 @@ var verbs = ["Calculating", "Computing", "Analyzing", "Generating", "Extrapolati
 var adjs = ["exponential", "underlying", "differential", "overloaded", "underloaded", "integrated", "abstract", "rigorous", "related", "similar", "logic", "formal", "natural", "virtual", "alternative", "derivable", "dedicated", "included", "excluded", "imported", "exported"];
 var nouns = ["matrix", "values", "data", "search engines", "indices", "key/value pairs", "RDF triples", "numbers", "quantities", "structures", "patterns", "measures", "conclusion", "abstraction", "systems", "metadata", "theory", "language", "axioms"];
 
+// Random color generation
+var colors = ["primary", "success", "warning", "danger", "info"];
+
 function getRandomElement(items){
     return items[Math.floor(Math.random()*items.length)];
 }
@@ -43,6 +48,7 @@ function getLoadingMessage(){
     return [verb, adj, noun].join(" ") + "...";
 }
 
+
 $(function(){
 	var Elements = {
 		$search : $("#search"),
@@ -50,11 +56,31 @@ $(function(){
 		$results : $("#results"),
 		$graph : $("#graph"),
 		$searchOptions : $("#searchOptions"),
-		$progressMsg : $("#progressMsg")
+		$progressMsg : $("#progressMsg"),
+		$progressBar : $("#progressBar")
+	};
+
+	var timeout = null;
+
+	var newLoading = function(){
+		var minTimer = 1;
+		var maxTimer = 10;
+		var timer = (Math.floor(Math.random()*(maxTimer-minTimer))+minTimer)*1000;
+		var color = getRandomElement(colors);
+		var $bar = $('<div class="progress-bar progress-bar-'+color+'"></div>').css("transition-duration",(timer/1000)+"s");
+
+		Elements.$progressMsg.text(getLoadingMessage());
+		Elements.$progressBar.html($bar);
+		// laisser le $bar.width
+		$bar.width();
+		$bar.addClass("load");
+
+		timeout = setTimeout(function(){
+			newLoading();
+		},timer);
 	};
 
 	var States = {
-		interval : null,
 		init: function(){
 			Elements.$search.removeClass("small");
 			Elements.$loader.addClass("hide");
@@ -68,12 +94,11 @@ $(function(){
 			Elements.$results.addClass("hide");
 			Elements.$graph.addClass("hide");
 			Elements.$searchOptions.collapse("hide");
-			setInterval(function(){
-				States.interval = Elements.$progressMsg.text(getLoadingMessage());
-			}, 3000);
+			newLoading();
+			//setTimeout(States.displayResults,20*1000);
 		},
 		displayResults: function(){
-			clearInterval(States.interval);
+			clearTimeout(timeout);
 			Elements.$search.addClass("small");
 			Elements.$loader.addClass("hide");
 			Elements.$results.removeClass("hide");
@@ -104,6 +129,7 @@ $(function(){
 		params.append_keyword = $("input[name='appendKeyword']:checked").length?"true":"false";
 
 		States.loading();
+		/*
 		$.ajax({
 			method: "GET",
 			url: path,
@@ -123,6 +149,7 @@ $(function(){
 				console.log(response);
 			}
 		});
+		*/
 		return false;
 	});
 
@@ -131,4 +158,6 @@ $(function(){
 	// Run
 	States.init();
 });
+
+
 
